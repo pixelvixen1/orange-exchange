e9654cba9f7879b674538086ad10a151c4de684b
angel777 <publicisangel@gmail.com>
2014-01-24 18:56:19 +0000

    added initial deployment files

---
+++ DIR39/deploy/index.php
@@ -0,0 +1,9 @@
+<?php
+$version    = "dir"; // TYPE VERSION NAME EG: rdm or rds
+$issue_num  = "39"; // ISSUE NUMBER ONLY EG: 24, 25, 26 ETC
+$flash_file = "growth"; // FLASH FILE NAME EG: retention or growth 
+$header_link = 'Jan/Feb 2014';
+
+include("../oeskin/html/header.php"); 
+include("../oeskin/html/footer.php"); 
+?>
